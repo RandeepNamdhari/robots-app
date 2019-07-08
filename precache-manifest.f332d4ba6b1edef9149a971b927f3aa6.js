9dc5ee16a6ab73364a2b9f966ca32bc0a397ad87
RandeepNamdhari <randeepsinghdev87@gmail.com>
2019-07-08 15:18:29 +0530

    Updates

---
+++ precache-manifest.f332d4ba6b1edef9149a971b927f3aa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc75ce2da069819951631c24828445a",
+    "url": "/robots-app/index.html"
+  },
+  {
+    "revision": "9f902feb94d814790364",
+    "url": "/robots-app/static/css/main.30814368.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/robots-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9f902feb94d814790364",
+    "url": "/robots-app/static/js/main.a2875608.chunk.js"
+  },
+  {
+    "revision": "2f8db4935a31351d7bd2",
+    "url": "/robots-app/static/js/runtime~main.cb3fb40b.js"
+  }
+]);
